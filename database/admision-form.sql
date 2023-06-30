f877622714af898cfc600ab0fd4994e147e75932
irudayan <irudayan111@gmail.com>
2023-06-30 10:41:41 +0530

    database
    database update

--- database/admision-form.sql
+++ database/admision-form.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2023 at 11:17 AM
+-- Generation Time: Jun 30, 2023 at 07:09 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -112,6 +112,7 @@ CREATE TABLE `admisionforms` (
   `General_Course_3` text DEFAULT NULL,
   `General_Course_4` text DEFAULT NULL,
   `General_Course_5` text DEFAULT NULL,
+  `status` enum('Pending','Approved') NOT NULL DEFAULT 'Pending',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -120,9 +121,10 @@ CREATE TABLE `admisionforms` (
 -- Dumping data for table `admisionforms`
 --
 
-INSERT INTO `admisionforms` (`id`, `user_id`, `fullname`, `mobileno`, `bloodgroup`, `email`, `dateofbirth`, `fathername`, `mothername`, `religion`, `motherphone`, `fatherphone`, `fatheroccupation`, `motheroccupation`, `permanentaddress`, `correspondentaddress`, `localguardianname`, `localguardianno`, `category`, `tribe`, `postcode`, `gender`, `country`, `state`, `hslcyear`, `hslcrollno`, `hslcdivision`, `hslcpercentage`, `hslcboard`, `hsslcyear`, `hsslcrollno`, `hsslcdivision`, `hsslcpercentage`, `hsslcboard`, `schoolname`, `collegename`, `schoollastattended`, `collegelastattended`, `boarduniversity`, `cerificateno`, `certificatedate`, `subject1`, `subject2`, `subject3`, `subject4`, `subject5`, `previouspercentage`, `previousgrade`, `photo`, `hsslcmarksheet`, `hsslcadmitcard`, `birthcertificate`, `communitycertificate`, `DSE_English`, `DSE_History`, `DSE_Philosophy`, `DSE_Pol_Science`, `DSE_Sociology`, `GE_English`, `GE_History`, `GE_Philosophy`, `GE_Pol_Science`, `GE_Pol-Science`, `GE_Sociology`, `SEC_English`, `SEC_History`, `SEC_Philosophy`, `SEC_Pol_Science`, `SEC_Sociology`, `AECC_English`, `AECC_History`, `AECC_Philosophy`, `AECC_Pol_Science`, `AECC_Sociology`, `Hon_Sub_1`, `Hon_Sub_2`, `Hon_Sub_3`, `Hon_Sub_4`, `Hon_Sub_5`, `General_Course_1`, `General_Course_2`, `General_Course_3`, `General_Course_4`, `General_Course_5`, `created_at`, `updated_at`) VALUES
-(1, 8, 'RAJ', '768677677877', 'B+', 'irudayan111@gmail.com', '27.06.2023', 'savariyappan', 'Thripattur', 'Buddhist', '08760870314', '08760870314', 'cooli', 'cooli', 'Thripattur', 'Visuvasampatti', 'Thripattur', '8760870314', 'Thripattur', 'sc', '635307', 'Male', 'India', 'Maharastra', '1998', '3457', '565', 'Thripattur', 'Thripattur', '2003', '67', '634', '56', 'Thripattur', 'dominicc savio', 'sacred heart college', '12', 'Thripattur', 'Thripattur', '9042379218', '5/08/2003', '12', '21', '23', '34', '34', '66', 'A+', 'church.jpeg', 'bg.png', NULL, 'bg.png', 'bg.png', NULL, 'DSE_English4', 'DSE_English4', 'DSE_English', 'DSE_English', 'DSE_English2', 'DSE_English4', 'DSE_English4', 'DSE_English', NULL, 'DSE_English', 'DSE_English3', 'DSE_English4', 'DSE_English4', 'DSE_English', 'DSE_English', 'DSE_English4', 'DSE_English4', 'DSE_English', 'DSE_English', 'DSE_English', 'bg.png', 'rettyty', 'bg.png', 'bg.png', 'bg.png', 'bg.png', 'bg.png', 'bg.png', 'bg.png', 'bg.png', '2023-06-26 11:32:51', '2023-06-26 22:42:32'),
-(2, 9, 'raj', '08760870314', 'B+', 'irudayan111@gmail.com', '29.06.2023', 'savariyappan', 'Thripattur', 'Hindu', '9042379218', '08760870314', 'manager', 'manager', 'Thripattur', 'thruvannamalai', 'raj', '8760870314', 'raj', 'bc', '635307', 'Female', 'America', 'TamilNadu', '1998', '64', '567', '67', 'board', '2003', '67', '634', '56', 'board', 'dominicc savio', 'sacred heart college', '12', 'Thripattur', 'Thripattur', '9042379218', '5/08/2003', NULL, '21', '23', '34', '34', '66', 'A+', 'bg.png', 'வி.மாதேப்பள்ளி.png', 'வி.மாதேப்பள்ளி.png', 'வி.மாதேப்பள்ளி.png', 'பெரிய ஏறிகோடி.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2023-06-27 03:26:05', '2023-06-27 03:26:05');
+INSERT INTO `admisionforms` (`id`, `user_id`, `fullname`, `mobileno`, `bloodgroup`, `email`, `dateofbirth`, `fathername`, `mothername`, `religion`, `motherphone`, `fatherphone`, `fatheroccupation`, `motheroccupation`, `permanentaddress`, `correspondentaddress`, `localguardianname`, `localguardianno`, `category`, `tribe`, `postcode`, `gender`, `country`, `state`, `hslcyear`, `hslcrollno`, `hslcdivision`, `hslcpercentage`, `hslcboard`, `hsslcyear`, `hsslcrollno`, `hsslcdivision`, `hsslcpercentage`, `hsslcboard`, `schoolname`, `collegename`, `schoollastattended`, `collegelastattended`, `boarduniversity`, `cerificateno`, `certificatedate`, `subject1`, `subject2`, `subject3`, `subject4`, `subject5`, `previouspercentage`, `previousgrade`, `photo`, `hsslcmarksheet`, `hsslcadmitcard`, `birthcertificate`, `communitycertificate`, `DSE_English`, `DSE_History`, `DSE_Philosophy`, `DSE_Pol_Science`, `DSE_Sociology`, `GE_English`, `GE_History`, `GE_Philosophy`, `GE_Pol_Science`, `GE_Pol-Science`, `GE_Sociology`, `SEC_English`, `SEC_History`, `SEC_Philosophy`, `SEC_Pol_Science`, `SEC_Sociology`, `AECC_English`, `AECC_History`, `AECC_Philosophy`, `AECC_Pol_Science`, `AECC_Sociology`, `Hon_Sub_1`, `Hon_Sub_2`, `Hon_Sub_3`, `Hon_Sub_4`, `Hon_Sub_5`, `General_Course_1`, `General_Course_2`, `General_Course_3`, `General_Course_4`, `General_Course_5`, `status`, `created_at`, `updated_at`) VALUES
+(1, 9, 'savari savari', '08760870314', 'O-', 'irudayan111@gmail.com', '30.06.2023', 'savariyappan', 'Thripattur', NULL, '08760870314', '08760870314', NULL, NULL, 'Thripattur', NULL, NULL, NULL, NULL, NULL, NULL, 'Female', 'India', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'வி.மாதேப்பள்ளி.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Approved', '2023-06-29 11:28:21', '2023-06-29 23:02:30'),
+(2, 11, 'raj', '08760870314', 'AB-', 'irudayan111@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Thripattur', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'India', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'பெரிய ஏறிகோடி.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Approved', '2023-06-29 11:43:13', '2023-06-29 23:05:27'),
+(3, 13, 'noThripattur', '08760870314', 'AB+', 'irudayan111@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Thripattur', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'America', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'bg.png', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'DSE_English', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Pending', '2023-06-29 11:43:53', '2023-06-29 12:16:37');
 
 -- --------------------------------------------------------
 
@@ -162,7 +164,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2014_10_12_100000_create_password_resets_table', 1),
 (4, '2019_08_19_000000_create_failed_jobs_table', 1),
 (5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(12, '2023_06_22_091156_create_admisionforms_table', 2);
+(14, '2023_06_22_091156_create_admisionforms_table', 2);
 
 -- --------------------------------------------------------
 
@@ -231,9 +233,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `usertype`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', 'irudayan111@gmail.com', 'Admin', NULL, '$2y$10$BSYB/r.5z7av3sdK9ADqgejLHt80nyewQDYPcDtQdzE9/CO5dFlqa', NULL, '2023-06-19 23:52:38', '2023-06-21 06:20:48'),
-(8, 'raj', 'raj@gamil.com', 'Users', NULL, '$2y$10$MHxo/a1SRcp2ud8awDQr7.p4Cyly0rT47/07uQOjdTRTzZ7pNue7K', NULL, '2023-06-23 11:42:28', '2023-06-23 11:42:28'),
-(9, 'an', 'and@gmail.com', 'Users', NULL, '$2y$10$HnOwa.XLXKImK4GjhKnLl.UFv.Up32MLn0PHGLA8Nxmc/NxBGdBkC', NULL, '2023-06-23 23:30:29', '2023-06-23 23:30:29'),
-(10, 'sr', 'sr@gmail.com', 'Users', NULL, '$2y$10$kq4deVVFVDGlMA6HPDaYKun9/jWBp6aAGUtqTwcUVZyXk1yUyUMkW', NULL, '2023-06-24 01:58:50', '2023-06-24 01:58:50');
+(9, 'an', 'and@gmail.com', 'Users', NULL, '$2y$10$HnOwa.XLXKImK4GjhKnLl.UFv.Up32MLn0PHGLA8Nxmc/NxBGdBkC', 'AHZf7J3tQob2DIBZ6U0ZS4uF0CyST1V8j7qxGuPq0afKZQcEQ2xL9IhA4wBB', '2023-06-23 23:30:29', '2023-06-23 23:30:29'),
+(11, 'yes@gmail.com', 'yes@gmail.com', 'Users', NULL, '$2y$10$wBKXaQgE4IEl5JIa7fV91OJd1mpl99l0D2DLcprnED4uMTXg6ggPG', NULL, '2023-06-27 04:37:08', '2023-06-27 04:37:08'),
+(12, 'savari savari', 'irudayan1@gmail.com', 'Users', NULL, '$2y$10$AhT5MEL/IMsjytUpcLIT6.7GKqWaM9ksTFF6T55x.W3q7JksothlG', NULL, '2023-06-27 04:48:33', '2023-06-27 04:48:33'),
+(13, 'savari savari', 'no@gmail.com', 'Users', NULL, '$2y$10$CzZqGfNxS6ppzMicmHcVceIJGUnY1pRCm4R7m5w3Z7fqusrdRlVtS', NULL, '2023-06-27 05:01:56', '2023-06-27 05:01:56');
 
 --
 -- Indexes for dumped tables
@@ -293,7 +296,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admisionforms`
 --
 ALTER TABLE `admisionforms`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -305,7 +308,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -317,7 +320,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
